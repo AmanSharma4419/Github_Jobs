e3c5d989cb23a91e7ad8c0c6f9c797c0a70e0251
AmanSharma4419 <sharmaaman4419@gmail.com>
2020-06-23 11:52:31 +0545

    Added loader

--- src/components/AllJobs.jsx
+++ src/components/AllJobs.jsx
@@ -2,10 +2,9 @@ import React, { useState, useEffect } from "react";
 import FetchJobs from "./FetchingJobs";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";  
 import Typography from "@material-ui/core/Typography";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 const useStyles = makeStyles({
   root: {
@@ -24,12 +23,14 @@ const useStyles = makeStyles({
   },
 });
 export default function AllJobs() {
-  const [initialState, setinitialState] = useState({ jobsList: "" });
-
+  const [initialState, setinitialState] = useState({
+    jobsList: "",
+  });
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     FetchJobs.FetchGithubJobs().then(
       (allJobs) => setinitialState((initialState.jobsList = allJobs)),
-      console.log(initialState)
+      setIsLoading(false)
     );
   }, []);
   return (
@@ -40,38 +41,53 @@ export default function AllJobs() {
           Object.keys(initialState).map((key, index) => {
             return (
               <>
-                <Card className={useStyles.root}>
-                  <CardContent>
-                    <Typography
-                      className={useStyles.title}
-                      color="textSecondary"
-                      gutterBottom
-                    >
-                      <strong>Company Name</strong> :-
-                      {initialState[key].company}
-                    </Typography>
+                {isLoading ? (
+                  <div style={textAlignment}>
+                    <CircularProgress />
+                  </div>
+                ) : (
+                  <>
+                    <Card className={useStyles.root}>
+                      <CardContent>
+                        <Typography
+                          className={useStyles.title}
+                          color="textSecondary"
+                          gutterBottom
+                        >
+                          <strong>Company Name</strong> :-
+                          {initialState[key].company}
+                        </Typography>
 
-                    <Typography className={useStyles.pos} color="textSecondary">
-                      <a
-                        className={useStyles.pos}
-                        href={initialState[key].company_url}
-                      >
-                        <strong>Company Url</strong> :-
-                      </a>
-                      {initialState[key].company_url}
-                    </Typography>
-                    <Typography color="textSecondary" gutterBottom>
-                      <strong>Title</strong> :- {initialState[key].title}
-                    </Typography>
-                    <Typography color="textSecondary" gutterBottom>
-                      <strong> Location</strong>:- {initialState[key].location}
-                      <br />
-                    </Typography>
-                    <Typography color="textSecondary" gutterBottom>
-                      <strong> Type</strong> :- {initialState[key].type}
-                    </Typography>
-                  </CardContent>
-                </Card>
+                        <Typography
+                          className={useStyles.pos}
+                          color="textSecondary"
+                        >
+                          <a
+                            className={useStyles.pos}
+                            href={initialState[key].company_url}
+                          >
+                            <strong>Company Url</strong> :-
+                          </a>
+                          {initialState[key].company_url
+                            ? initialState[key].company_url
+                            : "Url is not metioned"}
+                        </Typography>
+                        <Typography color="textSecondary" gutterBottom>
+                          <strong>Title</strong> :- {initialState[key].title}
+                        </Typography>
+                        <Typography color="textSecondary" gutterBottom>
+                          <strong> Location</strong>:-{" "}
+                          {initialState[key].location}
+                          <br />
+                        </Typography>
+                        <Typography color="textSecondary" gutterBottom>
+                          <strong> Type</strong> :- {initialState[key].type}
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                    <br />
+                  </>
+                )}
               </>
             );
           })}
