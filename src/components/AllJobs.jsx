ee0b9dfa53731ffd6f3065b8feb9bea795a6e19a
AmanSharma4419 <sharmaaman4419@gmail.com>
2020-06-23 09:53:24 +0545

    Minor chages in listing jobs

--- src/components/AllJobs.jsx
+++ src/components/AllJobs.jsx
@@ -1,19 +1,28 @@
 import React, { useState, useEffect } from "react";
 import FetchJobs from "./FetchingJobs";
 import { makeStyles } from "@material-ui/core/styles";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import Divider from "@material-ui/core/Divider";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";  
+import Typography from "@material-ui/core/Typography";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles({
   root: {
-    width: "100%",
-    maxWidth: 360,
-    backgroundColor: theme.palette.background.paper,
+    minWidth: 275,
   },
-}));
-
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 18,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 export default function AllJobs() {
   const [initialState, setinitialState] = useState({ jobsList: "" });
 
@@ -26,38 +35,43 @@ export default function AllJobs() {
   return (
     <>
       <p style={textAlignment}>Here you can findout suitable jobs</p>
-      <div className={useStyles.root}>
+      <div>
         {initialState !== "" &&
           Object.keys(initialState).map((key, index) => {
             return (
               <>
-                <List component="nav" aria-label="main mailbox folders">
-                  <ListItem>
-                    Company Name
-                    <ListItemText primary={initialState[key].company} />
-                  </ListItem>
-                  <ListItem>
-                    Company Url
-                    <ListItemText primary={initialState[key].company_url} />
-                  </ListItem>
-                </List>
-                <ListItem>
-                  Company Location
-                  <ListItemText primary={initialState[key].location} />
-                </ListItem>
-                <ListItem>
-                  Company Title
-                  <ListItemText primary={initialState[key].title} />
-                </ListItem>
-                <ListItem>
-                  Job Type
-                  <ListItemText primary={initialState[key].type} />
-                </ListItem>
-                <ListItem>
-                  Apply
-                  <ListItemText primary={initialState[key].url} />
-                </ListItem>
-                <Divider />
+                <Card className={useStyles.root}>
+                  <CardContent>
+                    <Typography
+                      className={useStyles.title}
+                      color="textSecondary"
+                      gutterBottom
+                    >
+                      <strong>Company Name</strong> :-
+                      {initialState[key].company}
+                    </Typography>
+
+                    <Typography className={useStyles.pos} color="textSecondary">
+                      <a
+                        className={useStyles.pos}
+                        href={initialState[key].company_url}
+                      >
+                        <strong>Company Url</strong> :-
+                      </a>
+                      {initialState[key].company_url}
+                    </Typography>
+                    <Typography color="textSecondary" gutterBottom>
+                      <strong>Title</strong> :- {initialState[key].title}
+                    </Typography>
+                    <Typography color="textSecondary" gutterBottom>
+                      <strong> Location</strong>:- {initialState[key].location}
+                      <br />
+                    </Typography>
+                    <Typography color="textSecondary" gutterBottom>
+                      <strong> Type</strong> :- {initialState[key].type}
+                    </Typography>
+                  </CardContent>
+                </Card>
               </>
             );
           })}
